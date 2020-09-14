0ad61f6e9b5ca00104e258028c14fa62f4753d35
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-14 10:23:14 -0300

    adicionando cor de fundo e altura

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import { View, Text, AppRegistry } from 'react-native';
 
 const Estilos = {
     estiloTexto: {
-        fontSize: 50
+        fontSize: 30,
+        backgroundColor: '#08509B',
+        height: 80
     }
 };
 
