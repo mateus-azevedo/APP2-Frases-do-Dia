e3138e94e95c5f715c0491b1b6de9c3f21fe70e2
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-15 21:01:00 -0300

    algumas formatações de texto

--- index.js
+++ index.js
@@ -3,21 +3,27 @@ import { View, Text, AppRegistry } from 'react-native';
 
 const Estilos = {
     estiloTexto: {
-        fontSize: 20,
+        fontSize: 30,
         backgroundColor: '#08509B',
         paddingTop: 15,
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0, 
-            height: 2,
-        },
-        shadowOpacity: 0.4
+        color: '#fff',
+        fontStyle: 'italic',
+        fontWeight: '100',
+        textAlign: 'center',
+        textDecorationLine: 'underline'
+        
+        // shadowColor: '#000',
+        // shadowOffset: {
+        //     width: 0, 
+        //     height: 2,
+        // },
+        // shadowOpacity: 0.4
     }
 };
 
 const App = () => {
     return(
-        <View>
+        <View style={{elevation: 3}}>
             <Text style={ Estilos.estiloTexto }>Frases do dia</Text>
         </View>
     );
