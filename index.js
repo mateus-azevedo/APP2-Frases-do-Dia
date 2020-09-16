6d82260b9aaeddd5deb898e8da4b25b47d4af573
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-15 21:42:14 -0300

    funcionamento do alignItems: stretch

--- index.js
+++ index.js
@@ -5,24 +5,25 @@ const Estilos = {
     estiloTexto: {
         fontSize: 30,
         backgroundColor: '#08509B',
-        height: 60,
-        width: 60,
+        // height: 60,   //Tirando o tamanho fixo dos componentes da minha View
+        // width: 60,
         textAlign: 'center',
         textAlignVertical: 'center'
     },
     estiloTexto2: {
         fontSize: 30,
         backgroundColor: '#2A48FA',
-        height: 60,
-        width: 60,
+        // height: 60,   //Tirando o tamanho fixo dos componentes da minha View
+        // width: 60,
         textAlign: 'center',
         textAlignVertical: 'center'
     },
     estiloView: {
         backgroundColor: 'skyblue',
-        height: 300,
-        justifyContent: 'center',
-        alignItems: 'flex-end'
+        height: 600,
+        justifyContent: 'space-between',
+        alignItems: 'stretch', //stretch é o valor padrão do alignItems. Só funciona quando não tenho um tamanho fixo para os componentes dentro da View
+        flexDirection: 'column'
     }
 };
 
@@ -32,6 +33,7 @@ const App = () => {
         <View style={ estiloView }>
             <Text style={ estiloTexto }>A</Text>
             <Text style={ estiloTexto2 }>B</Text>
+            <Text style={ estiloTexto }>C</Text>
         </View>
     );
 };
