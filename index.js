defeedbe1742bf7f6286eb6d1552767f36e8925d
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-22 16:15:47 -0300

    tentativa de adicionar texto a imagem(seguindo a aula)

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const Estilos = {
     
     principal: {
         paddingTop: 40
+    },
+    imagem: {
+        justifyContent: 'center'
     }
     
 };
 
 const App = () => {
-    const { principal } = Estilos;
+    const { principal, imagem } = Estilos;
     return(
         <View style={principal}>
 
-            <Image source={{uri: 'https://reactjs.org/logo-og.png'}}
-                   style={{width: 400, height: 400}} />
+            <Image style={imagem} source={ require('./imgs/uvas.png') } >
+                <Text>Legenda para a foto</Text>
+            </Image>
             
         </View>
     );
