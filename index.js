ea2fc12497ef4d106644c0b9ede3bfacad8e501d
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-22 17:01:11 -0300

    primeira interface do app

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { AppRegistry, View, Text, Image } from 'react-native';
+import { AppRegistry, View, Text, Image, TouchableOpacity } from 'react-native';
 
 const Estilos = {
     
     principal: {
-        backgroundColor: '#538530',
-        flex: 1
+        backgroundColor: '#ff1',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
+    botao: {
+        backgroundColor: '#538530',
+        paddingVertical: 10,
+        paddingHorizontal: 40,
+        marginTop: 20
+    },
+    textoBotao: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
     
 };
 
@@ -15,9 +28,14 @@ const botaoPressionado = () => {
 }
 
 const App = () => {
-    const { principal } = Estilos;
+    const { principal, botao, textoBotao } = Estilos;
     return(
         <View style={principal}>
+
+            <Image source={require('./imgs/logo.png')} />
+            <TouchableOpacity style={botao}>
+                <Text style={textoBotao}>Nova Frase</Text>
+            </TouchableOpacity>
         </View>
     );
 };
